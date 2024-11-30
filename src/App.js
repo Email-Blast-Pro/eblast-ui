c76a7ad5d29b93c76bc2072d61546668936e1a6a
danielortiz1982 <danielortiz1982@gmail.com>
2024-11-30 10:49:27 -0500

    create button component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Button } from "./components/button/button";
-import { Input } from "./components/input/input";
 
 function App() {
   return (
@@ -10,9 +9,77 @@ function App() {
       </header>
 
       <div className="content">
-        <Button />
-        <div style={{ width: "75%", margin: "0 auto" }}>
-          <Input />
+        <div className="button-example" style={{ margin: "20px 0" }}>
+          <h3>Buttons</h3>
+          <Button
+            btn="primary"
+            id={"primary-button"}
+            label="primary"
+            borderRadius={"5px"}
+          />
+          <Button
+            btn="secondary"
+            label={"secondary"}
+            type="button"
+            borderRadius={"5px"}
+          />
+          <Button
+            btn="success"
+            label={"success"}
+            type="button"
+            borderRadius={"5px"}
+          />
+          <Button
+            btn="danger"
+            label={"danger"}
+            type="button"
+            borderRadius={"5px"}
+          />
+          <Button
+            btn="warning"
+            label={"warning"}
+            type="button"
+            borderRadius={"5px"}
+          />
+          <Button
+            btn="info"
+            label={"info"}
+            type="button"
+            borderRadius={"5px"}
+          />
+          <Button
+            btn="light"
+            label={"light"}
+            type="button"
+            borderRadius={"5px"}
+          />
+          <Button
+            btn="dark"
+            label={"dark"}
+            type="button"
+            borderRadius={"5px"}
+          />
+        </div>
+        <div className="button-example" style={{ margin: "20px 0" }}>
+          <Button btn="primary-outline" label="Primary" borderRadius={"5px"} />
+
+          <Button
+            btn="secondary-outline"
+            label="Secondary"
+            borderRadius={"5px"}
+          />
+
+          <Button btn="success-outline" label="Success" borderRadius={"5px"} />
+
+          <Button btn="danger-outline" label="Danger" borderRadius={"5px"} />
+
+          <Button btn="warning-outline" label="Warning" borderRadius={"5px"} />
+
+          <Button btn="info-outline" label="Info" borderRadius={"5px"} />
+
+          <Button btn="light-outline" label="light" borderRadius={"5px"} />
+
+          <Button btn="dark-outline" label="dark" borderRadius={"5px"} />
         </div>
       </div>
 
--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,38 +1,22 @@
 import "./button.css";
 
-export const Button = () => {
+export const Button = ({
+  id,
+  type = "button",
+  label = "",
+  btn = "primary",
+  borderRadius,
+}) => {
   return (
     <div className="Button">
-      <div class="button-example">
-        <h1>Button Component</h1>
-        <button type="button" class="primary">
-          Primary
-        </button>
-        <button type="button" class="secondary">
-          Secondary
-        </button>
-        <button type="button" class="success">
-          Success
-        </button>
-        <button type="button" class="danger">
-          Danger
-        </button>
-        <button type="button" class="warning">
-          Warning
-        </button>
-        <button type="button" class="info">
-          Info
-        </button>
-        <button type="button" class="light">
-          Light
-        </button>
-        <button type="button" class="dark">
-          Dark
-        </button>
-        <p>
-          <a href="/">Return Home</a>
-        </p>
-      </div>
+      <button
+        className={`button ${btn}`}
+        type={type}
+        id={id}
+        style={{ borderRadius }}
+      >
+        {label}
+      </button>
     </div>
   );
 };
